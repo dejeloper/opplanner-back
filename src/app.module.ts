090ed92d40592928bd0ec653f5dc5562f67f367e
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-05-03 00:33:49 -0500

    Add TypeOrmModule and  connections to postgres database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
-import { TasksModule } from './modules/tasks/tasks.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5499,
+      username: 'dejeloper',
+      password: 'dejeloper123**',
+      database: 'opplanner_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
